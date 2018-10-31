e57e7044d9e0a2019775df68878fc565e4436496
William Bouchara <william.bouchara@epitech.eu>
2018-10-31 13:09:33 +0100

    fixed force nkro

--- keyboards/massdrop/ctrl/config.h
+++ keyboards/massdrop/ctrl/config.h
@@ -58,7 +58,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 )
 
 /* Force boot in NKRO mode */
-#define FORCE_NKRO
+#define FORCE_NKRO 1
 
 /*
  * Feature disable options
